75fb620044a226a27440a7b1b1d9abc5c5360273
Elías Milano <87205729+MilanoTester@users.noreply.github.com>
2024-09-23 03:09:27 +0000

    Nombres se muestran sólo en consola

--- src/app.js
+++ src/app.js
@@ -7,5 +7,29 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our", "your"];
+  let adj = [
+    "great",
+    "awesome",
+    "special",
+    "fantastic",
+    "supercalifragilisticoexpialidoso"
+  ];
+  let noun = ["costco", "bauru", "spirit", "Lazarus", "brazuca", "dolphin"];
+  let domainName = [];
+  let l = 0;
+
+  for (let i in pronoun) {
+    for (let j in adj) {
+      for (let k in noun) {
+        domainName[l] = pronoun[i] + adj[j] + noun[k] + ".com";
+        l++;
+      }
+    }
+  }
+
+  for (let m in domainName) {
+    document.getElementById("content").innerHTML = domainName[m];
+    console.log(domainName[m]);
+  }
 };
