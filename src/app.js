bcafcfd431548ff755cb52bc5e7659f5d012651f
Elías Milano <87205729+MilanoTester@users.noreply.github.com>
2024-09-23 19:25:27 +0000

    Agrego Domain Hacks a color. Ver HTMLgit add .

--- src/app.js
+++ src/app.js
@@ -9,27 +9,44 @@ window.onload = function() {
   //write your code here
   let pronoun = ["the", "our", "your"];
   let adj = [
-    "great",
+    "supercalifragilisticoexpialidoso",
     "awesome",
     "special",
-    "fantastic",
-    "supercalifragilisticoexpialidoso"
+    "fantastic"
   ];
-  let noun = ["bauru", "spirit", "Lazarus", "brazuca", "dolphin"];
+  let noun = ["bau", "spir", "Lazar", "brazu", "dolph"];
+  let extensions = ["ru", "it", "us", "ca", "in"];
   let domainNames = [];
-  let l = 0;
+  let m = 0;
 
   for (let i in pronoun) {
     for (let j in adj) {
       for (let k in noun) {
-        domainNames[l] = pronoun[i] + adj[j] + noun[k] + ".com";
-        l++;
+        for (let l in extensions) {
+          domainNames[m] = pronoun[i] + adj[j] + noun[k] + "." + extensions[l];
+          m++;
+        }
       }
     }
   }
 
   let domainNamesHtml = domainNames.map(name => {
-    return '<li class="list-group-item alert-success mt-1">' + name + "</li>";
+    let html;
+    if (
+      name.includes("bau.ru") ||
+      name.includes("spir.it") ||
+      name.includes("Lazar.us") ||
+      name.includes("brazu.ca") ||
+      name.includes("dolph.in")
+    ) {
+      html =
+        '<li class="list-group-item list-group-item-success mt-1">' +
+        name +
+        "</li>";
+    } else {
+      html = '<li class="list-group-item mt-1">' + name + "</li>";
+    }
+    return html;
   });
 
   let domainNamesItem = document.getElementById("content");
